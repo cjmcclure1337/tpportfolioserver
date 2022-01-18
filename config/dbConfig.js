7eae9291348aee5449a8c0314de8e55d46bc2fe6
CJMcClure <cjmcclure1337@gmail.com>
2022-01-18 15:52:52 -0600

    Changed db to Heroku

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 // store the configuration secrets of the PG DB here
 module.exports = {
-    HOST: 'portfolio-database.c8y9msgkwpjh.us-east-1.rds.amazonaws.com', // your endpoint
-    USER: 'cjmcclure', // your username
-    PASSWORD: 'PortServer', // your password
-    DB: 'portfolio', // your db name
+    HOST: 'ec2-3-225-41-234.compute-1.amazonaws.com', // your endpoint
+    USER: 'eniyrtreaaypyi', // your username
+    PASSWORD: '6848c2ea44db8a2f6bc6f0a42cbb95011b0df8ef12d5f2c2d8d4a24babbb5392', // your password
+    DB: 'd9k39qph9el6jn', // your db name
     dialect: 'postgres',
     }
--- models/index.js
+++ models/index.js
@@ -1,14 +1,29 @@
 const dbConfig = require('../config/dbConfig.js')
 const { Sequelize, DataTypes } = require('sequelize')
 
-//Initialize Sequelize using imported configurations
-const sequelize = new Sequelize({
-    database:dbConfig.DB,
-    username: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    dialect: dbConfig.dialect,
-    host: dbConfig.HOST
-})
+// construct the sequelize object using the constructor
+let sequelize = null;
+
+if (process && process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+            }
+            }
+        }
+    );
+} else {
+    sequelize = new Sequelize(
+    { // use imported configurations from dbConfig
+        database: dbConfig.DB,
+        username: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        dialect: dbConfig.dialect,
+        host: dbConfig.HOST,
+    })
+}
 
 //Test connection to DB
 sequelize.authenticate()
