13883ac39328da3f4feca3abe30e29c07a3fea7e
CJMcClure <cjmcclure1337@gmail.com>
2022-01-18 15:56:07 -0600

    Moved config variables to environment

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 // store the configuration secrets of the PG DB here
 module.exports = {
-    HOST: 'ec2-3-225-41-234.compute-1.amazonaws.com', // your endpoint
-    USER: 'eniyrtreaaypyi', // your username
-    PASSWORD: '6848c2ea44db8a2f6bc6f0a42cbb95011b0df8ef12d5f2c2d8d4a24babbb5392', // your password
-    DB: 'd9k39qph9el6jn', // your db name
+    HOST: process.env.HOST, // your endpoint
+    USER: process.env.USER, // your username
+    PASSWORD: process.env.PASSWORD, // your password
+    DB: process.env.DB, // your db name
     dialect: 'postgres',
     }
