ffdb29e924108ae1e3a39b3ed9ac27550f7aae15
CJMcClure <cjmcclure1337@gmail.com>
2022-01-25 10:58:14 -0600

    CORS

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors")
 
 var indexRouter = require('./routes/index');
 var positionRouter = require('./routes/positions');
 var portfolioRouter = require('./routes/portfolio')
 
 var app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
