e0e6e3ce6ed20839e10efa05090cd7e1eae61d2d
CJMcClure <cjmcclure1337@gmail.com>
2022-01-26 11:02:10 -0600

    Added local testing functionality

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,9 @@
 // store the configuration secrets of the PG DB here
 module.exports = {
-    HOST: process.env.HOST, // your endpoint
-    USER: process.env.USER, // your username
-    PASSWORD: process.env.PASSWORD, // your password
-    DB: process.env.DB, // your db name
+    HOST: "ec2-3-225-41-234.compute-1.amazonaws.com", // your endpoint
+    USER: "eniyrtreaaypyi", // your username
+    PASSWORD: "6848c2ea44db8a2f6bc6f0a42cbb95011b0df8ef12d5f2c2d8d4a24babbb5392", // your password
+    DB: "d9k39qph9el6jn", // your db name
     dialect: 'postgres',
+    URL: "postgres://eniyrtreaaypyi:6848c2ea44db8a2f6bc6f0a42cbb95011b0df8ef12d5f2c2d8d4a24babbb5392@ec2-3-225-41-234.compute-1.amazonaws.com:5432/d9k39qph9el6jn"
     }
--- models/index.js
+++ models/index.js
@@ -7,22 +7,27 @@ let sequelize = null;
 if (process && process.env.DATABASE_URL) {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-            }
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
             }
         }
-    );
+    });
 } else {
-    sequelize = new Sequelize(
+    sequelize = new Sequelize( dbConfig.URL,
     { // use imported configurations from dbConfig
         database: dbConfig.DB,
         username: dbConfig.USER,
         password: dbConfig.PASSWORD,
         dialect: dbConfig.dialect,
         host: dbConfig.HOST,
-    })
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    },)
 }
 
 //Test connection to DB
